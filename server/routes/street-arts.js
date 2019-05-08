19bed62ee36a65b29f86d7d79cc249d94a59aeff
boss1104 <alphaBoss1104@gmail.com>
2019-05-08 12:30:17 +0100

    iteration 4 and 5

--- server/routes/street-arts.js
+++ server/routes/street-arts.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const StreetArt = require('../models/StreetArt')
-
 const router = express.Router();
+const StreetArt = require('../models/StreetArt')
+const uploader = require('../configs/cloudinary')
+
 
 router.get('/', (req, res, next) => {
   StreetArt.find()
@@ -9,6 +10,32 @@ router.get('/', (req, res, next) => {
       res.json(Street);
     })
     .catch(err => next(err))
+});
+
+//Route to get a specific street art
+router.get('/:StreetArtId', (req, res, next) => {
+  StreetArt.findById(req.params.StreetArtId)
+    .then(Street => {
+      res.json(Street)
+    })
+    .catch(err => next(err))
+});
+
+// Route to create a street art
+// `uploader.single('picture')` parses the data send with the name `picture` and save information inside `req.file`
+router.post('/', uploader.single('picture'), (req, res, next) => {
+  let { lat, lng } = req.body
+  let pictureUrl = req.file.url
+  StreetArt.create({
+    pictureUrl,
+    location: {coordinates:[lat, lng]}
+  })
+  .then (response => {
+    res.json(response)
+  })
+  .catch(err => {
+    res.json(err)
+  })
 });
 
 module.exports = router;
